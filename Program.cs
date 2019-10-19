c38a4fe2efd3577b236c8620c83d3cc90598fa9f
ilyabelyaninov <56212974+ilyabelyaninov@users.noreply.github.com>
2019-10-19 20:49:05 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] nums = new int[4, 5];
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i++)
+                for (int k = 0; k < 5; k++)
+                    nums[i, k] = -(i + k);
+
+            foreach (int l in nums)
+            {
+                Console.WriteLine(l);
+            }
+            Console.ReadKey();
+        }
+    }
+}
